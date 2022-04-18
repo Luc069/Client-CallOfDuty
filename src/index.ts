cbc695876b6bf1572662ea70006a2a67d65724b3
Luc069 <91779803+Luc069@users.noreply.github.com>
2022-04-18 17:35:13 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,64 @@
-import { IncomingHttpHeaders } from "http";
-import { request } from "undici";
+const userAgent: string =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+let baseCookie: string = "new_SiteId=cod; ACT_SSO_LOCALE=en_US;country=US;"
+let baseSsoToken: string = ""
 
-const userAgent: string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
-let baseCookie: string = "new_SiteId=cod; ACT_SSO_LOCALE=en_US;country=US;";
-let baseSsoToken: string = '';
-
-interface CustomHeaders extends IncomingHttpHeaders {
-    "X-XSRF-TOKEN"?: string | undefined;
-    "X-CSRF-TOKEN"?: string | undefined;
-    "Atvi-Auth"?: string | undefined;
-    "ACT_SSO_COOKIE"?: string | undefined;
-    "atkn"?: string | undefined;
-    'cookie'?: string | undefined;
-    'content-type'?: string | undefined;
-};
-
-let baseHeaders: CustomHeaders = { 
-    'content-type': 'application/json',
-    'cookie': baseCookie,
-    'user-agent': userAgent
-};
-
-let baseUrl: string = "https://my.callofduty.com";
-let apiPath: string = "/api/papi-client";
-let loggedIn: boolean = false;
-
-enum platforms  {
-    All = 'all',
-    Activision = 'acti',
-    Battlenet = 'battle',
-    PSN = 'psn',
-    Steam = 'steam',
-    Uno = 'uno',
-    XBOX = 'xbl'
-};
-
-const sendRequest = async (url: string) => {
-    try {
-        if (!loggedIn) throw new Error("Not Logged In.");
-        let requestUrl = `${baseUrl}${apiPath}${url}`;
-        const { body, statusCode } = await request(requestUrl, {
-            headers: baseHeaders
-        });
-        
-        let response = await body.json();
-
-        if (statusCode > 299) return response;
-
-        return response;
-    }
-    catch (exception: unknown) {
-        throw exception;
-    }
-};
-
-const cleanClientName = (gamertag: string): string => {
-    return encodeURIComponent(gamertag);
+let baseHeaders = {
+  "content-type": "application/json",
+  cookie: baseCookie,
+  "user-agent": userAgent,
 }
 
-const login = (ssoToken: string): boolean => {
-    if (!ssoToken || ssoToken.trim().length <= 0) return false;
-    let fakeXSRF = "68e8b62e-1d9d-4ce1-b93f-cbe5ff31a041";
-    baseHeaders["X-XSRF-TOKEN"] = fakeXSRF;
-    baseHeaders["X-CSRF-TOKEN"] = fakeXSRF;
-    baseHeaders["Atvi-Auth"] = ssoToken;
-    baseHeaders["ACT_SSO_COOKIE"] = ssoToken;
-    baseHeaders["atkn"] = ssoToken;
-    baseHeaders["cookie"] = `${baseCookie}ACT_SSO_COOKIE=${ssoToken};XSRF-TOKEN=${fakeXSRF};API_CSRF_TOKEN=${fakeXSRF};ACT_SSO_EVENT="LOGIN_SUCCESS:1644346543228";ACT_SSO_COOKIE_EXPIRY=1645556143194;comid=cod;ssoDevId=63025d09c69f47dfa2b8d5520b5b73e4;tfa_enrollment_seen=true;gtm.custom.bot.flag=human;`;
-    baseSsoToken = ssoToken;
-    loggedIn = true;
-    return loggedIn;
-};
+let baseUrl: string = "https://my.callofduty.com"
+let apiPath: string = "/api/papi-client"
+let loggedIn: boolean = false
+
+enum platforms {
+  All = "all",
+  Activision = "acti",
+  Battlenet = "battle",
+  PSN = "psn",
+  Steam = "steam",
+  Uno = "uno",
+  XBOX = "xbl",
+}
+
+export const sendRequest = async (url: string) => {
+  try {
+    let requestUrl = `${baseUrl}${apiPath}${url}`
+    const res = await fetch(requestUrl, {
+      headers: baseHeaders,
+    })
+
+    let data = await res.json()
+
+    if (data.statusCode > 299) return data
+
+    return data
+  } catch (exception: unknown) {
+    throw exception
+  }
+}
+
+const cleanClientName = (gamertag: string): string => {
+  return encodeURIComponent(gamertag)
+}
+
+export const login = async (ssoToken: string) => {
+  if (!ssoToken || ssoToken.trim().length <= 0) return false
+  let fakeXSRF = "68e8b62e-1d9d-4ce1-b93f-cbe5ff31a041"
+  baseHeaders["X-XSRF-TOKEN"] = fakeXSRF
+  baseHeaders["X-CSRF-TOKEN"] = fakeXSRF
+  baseHeaders["Atvi-Auth"] = ssoToken
+  baseHeaders["ACT_SSO_COOKIE"] = ssoToken
+  baseHeaders["atkn"] = ssoToken
+  baseHeaders[
+    "cookie"
+  ] = `${baseCookie}ACT_SSO_COOKIE=${ssoToken};XSRF-TOKEN=${fakeXSRF};API_CSRF_TOKEN=${fakeXSRF};ACT_SSO_EVENT="LOGIN_SUCCESS:1644346543228";ACT_SSO_COOKIE_EXPIRY=1645556143194;comid=cod;ssoDevId=63025d09c69f47dfa2b8d5520b5b73e4;tfa_enrollment_seen=true;gtm.custom.bot.flag=human;`
+  baseSsoToken = ssoToken
+  loggedIn = true
+  return loggedIn
+}
 
 class WZ {
     fullData = async (gamertag: string, platform: platforms) => {
